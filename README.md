99d61f33d762f05c0b1bbb4c7cd61dfe4afa5000
Tim Ribaric <tim.ribaric@gmail.com>
2014-10-23 11:46:38 -0400

    First working version
    Seems to do what it is supposed to.

---
+++ README.md
@@ -0,0 +1,9 @@
+
+#openurl_to_erm
+
+Purpose of this utility is to take a direct export from SFX and and resolve the final URL a full text journal object and munge is so that it can be directly imported to III ERM system
+Heavily inspired by U of Saskatchewan journal catalogue display
+
+1. Fill in details in *setting.org* and rename to *setting.py*
+1. Export as TXT with complete SFX metadata include
+1. Parse away (end product is in *final.txt*)
---
+++ openurl_to_erm.py
@@ -0,0 +1,48 @@
+
+
+#
+# openurl_to_erm
+#
+# Usage: python openurl_to_erm.py opens first txt it finds and resolves into final.txt
+#
+
+import os
+import string
+
+from settings import *
+#
+#
+#
+def resolve(sfxIn):
+
+  jdata = sfxIn.split('\t')
+  title = jdata[1]
+  issn = jdata[7].replace("-","")
+  url = BASE_URL+issn
+  ermReady = GENERIC_TARGET_NAME+SEPARATOR+title+SEPARATOR+issn+SEPARATOR+url+"\n"
+
+  return ermReady
+
+
+
+
+if __name__ == "__main__":
+  print "Resolving",
+  for f in os.listdir(os.getcwd()):
+    if f[-3:] == "txt" and f != "final.txt":
+      infile = open(f,"r")
+      break
+  if not infile:
+    print " input file not found"
+    exit
+  print ""
+
+
+outfile = open("final.txt","w")
+outfile.write(HEADING_LINE+"\n")
+for line in infile:
+  outfile.write(resolve(line))
+
+outfile.close()
+infile.close()
+print " fin"
